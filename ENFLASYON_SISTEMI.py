8feee3a4ea0ec038e2c32b7c159d045c1ad6838e
Validation2026 <validation2026@gmail.com>
2026-02-12 11:14:21 +0300

    Update ENFLASYON_SISTEMI.py

--- ENFLASYON_SISTEMI.py
+++ ENFLASYON_SISTEMI.py
@@ -944,7 +944,7 @@ def main():
     
     # --- AYAR: SENKRONİZASYON BUTONU ---
     # Bu ayarı False yaparak butonu tamamen gizleyebilirsiniz.
-    SENKRONIZASYON_AKTIF = True 
+    SENKRONIZASYON_AKTIF = False 
 
     # --- Üst Bilgi Barı (Sticky Header) ---
     st.markdown(f"""
@@ -1028,3 +1028,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
